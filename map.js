a44492f8019af58274961b19e9c476dabadeeb86
varGeneric <16168572+varGeneric@users.noreply.github.com>
2020-08-04 23:05:52 -0400

    Aug 4th

--- map.js
+++ map.js
@@ -1,7 +1,7 @@
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: -34.397, lng: 150.644},
-      zoom: 8
-    });
-    console.log("map made")
-  }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 0.0, lng: 0.0},
+    zoom: 2
+  });
+  console.log("map made")
+}
