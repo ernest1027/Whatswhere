9cf14387e9da20bedd0d8cbce9d33df12374301d
ernest1027 <ernestwong1027@gmail.com>
2020-08-08 00:02:23 -0400

    changes

--- crowdsourcePage.js
+++ crowdsourcePage.js
@@ -1,20 +1,11 @@
 function changeColor(){
 	let avail = document.getElementById("Availability").value;
-	if(avail==0){
-		document.getElementById("availContainer").style.backgroundColor = "red";
-	}
-	if(avail==1){
-		document.getElementById("availContainer").style.backgroundColor = "orange";
-	}
-	if(avail==2){
-		document.getElementById("availContainer").style.backgroundColor = "yellow";
-	}
-	if(avail==3){
-		document.getElementById("availContainer").style.backgroundColor = "green";
-	}
-	if(avail==4){
-		document.getElementById("availContainer").style.backgroundColor = "green";
-	}
+	let colorR = (255 - ((avail) *2.55))%1;
+	let colorG = (avail*2.55)%1;
+	let colorB = 0
+
+	document.getElementById("availContainer").style.color= (colorR, colorG, colorB); 
+	
 	
 }
 var a = document.getElementById("Availability");
