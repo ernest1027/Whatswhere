c39a38f21965d12c39109a621ef31345170ed724
varGeneric <16168572+varGeneric@users.noreply.github.com>
2020-08-15 23:26:10 -0400

    idk

--- crowdsourcePage.js
+++ crowdsourcePage.js
@@ -92,63 +92,63 @@ $(document).ready(function(){
 // // var a = document.getElementById("Availability");
 // // a.addEventListener('input', changeColor);
 
-// const $element = $('input[type="range"]');
-// const $tooltip = $('#range-tooltip');
-// const sliderStates = [
-//   {name: "1", tooltip: "<h2><strong>Out of stock</strong></h2>", range: _.range(0, 10) },
-//   {name: "2", tooltip: "<h2><strong>A few left</strong></h2>", range: _.range(10, 20)},
-//   {name: "3", tooltip: "<h2><strong>A good amount available</strong></h2>", range: _.range(20, 30)},
-//   {name: "4", tooltip: "<h2><strong>Plenty available</strong></h2>", range: _.range(30, 40)},
-//   {name: "5", tooltip: "<h2><strong>Fully in stock</strong></h2>", range: _.range(40, 50)},
-// ];
-// var currentState;
-// var $handle;
+const $element = $('input[type="range"]');
+const $tooltip = $('#range-tooltip');
+const sliderStates = [
+  {name: "1", tooltip: "<h2><strong>Out of stock</strong></h2>", range: _.range(0, 10) },
+  {name: "2", tooltip: "<h2><strong>A few left</strong></h2>", range: _.range(10, 20)},
+  {name: "3", tooltip: "<h2><strong>A good amount available</strong></h2>", range: _.range(20, 30)},
+  {name: "4", tooltip: "<h2><strong>Plenty available</strong></h2>", range: _.range(30, 40)},
+  {name: "5", tooltip: "<h2><strong>Fully in stock</strong></h2>", range: _.range(40, 50)},
+];
+var currentState;
+var $handle;
 
-// $element
-//   .rangeslider({
-//     polyfill: false,
-//     onInit: function() {
-//       $handle = $('.rangeslider__handle', this.$range);
-//       updateHandle($handle[0], this.value);
-//       updateState($handle[0], this.value);
-//     }
-//   })
-//   .on('input', function() {
-//     updateHandle($handle[0], this.value);
-//     checkState($handle[0], this.value);
-//   });
+$element
+  .rangeslider({
+    polyfill: false,
+    onInit: function() {
+      $handle = $('.rangeslider__handle', this.$range);
+      updateHandle($handle[0], this.value);
+      updateState($handle[0], this.value);
+    }
+  })
+  .on('input', function() {
+    updateHandle($handle[0], this.value);
+    checkState($handle[0], this.value);
+  });
 
-// // Update the value inside the slider handle
-// function updateHandle(el, val) {
-//   el.textContent = "";
-// }
+// Update the value inside the slider handle
+function updateHandle(el, val) {
+  el.textContent = "";
+}
 
-// // Check if the slider state has changed
-// function checkState(el, val) {
-//   // if the value does not fall in the range of the current state, update that shit.
-//   if (!_.contains(currentState.range, parseInt(val))) {
-//     updateState(el, val);
-//   }
-// }
+// Check if the slider state has changed
+function checkState(el, val) {
+  // if the value does not fall in the range of the current state, update that shit.
+  if (!_.contains(currentState.range, parseInt(val))) {
+    updateState(el, val);
+  }
+}
 
-// // Change the state of the slider
-// function updateState(el, val) {
-//   for (var j = 0; j < sliderStates.length; j++){
-//     if (_.contains(sliderStates[j].range, parseInt(val))) {
-//       currentState = sliderStates[j];
-//       // updateSlider();
-//     }
-//   }
-//   // If the state is high, update the handle count to read 50+
-//   if (currentState.name == "high") {
-//     updateHandle($handle[0], "50+");
-//   }
-//   // Update handle color
-//   $handle
-//     .removeClass (function (index, css) {
-//     return (css.match (/(^|\s)js-\S+/g) ||   []).join(' ');
-//   })
-//     .addClass("js-" + currentState.name);
-//   // Update tooltip
-//   $tooltip.html(currentState.tooltip);
-// }
+// Change the state of the slider
+function updateState(el, val) {
+  for (var j = 0; j < sliderStates.length; j++){
+    if (_.contains(sliderStates[j].range, parseInt(val))) {
+      currentState = sliderStates[j];
+      // updateSlider();
+    }
+  }
+  // If the state is high, update the handle count to read 50+
+  if (currentState.name == "high") {
+    updateHandle($handle[0], "50+");
+  }
+  // Update handle color
+  $handle
+    .removeClass (function (index, css) {
+    return (css.match (/(^|\s)js-\S+/g) ||   []).join(' ');
+  })
+    .addClass("js-" + currentState.name);
+  // Update tooltip
+  $tooltip.html(currentState.tooltip);
+}
--- submitted.js
+++ submitted.js
@@ -1,14 +1,14 @@
 var urlParams;
 (window.onpopstate = function () {
     var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        pl = /\+/g,  // Regex for replacing addition symbol with a space
         search = /([^&=]+)=?([^&]*)/g,
         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
+        query = window.location.search.substring(1);
 
     urlParams = {};
     while (match = search.exec(query))
-       urlParams[decode(match[1])] = decode(match[2]);
+        urlParams[decode(match[1])] = decode(match[2]);
 })();
 
 
@@ -22,38 +22,34 @@ function addFood(food) {
     var store = urlParams.myStore;
     var foods = urlParams.myFood;
     var avail = parseInt(urlParams.Availability, 10);
-    
-    firebase.database().ref("/Foods/" + foods + "/Locations/" + store + "/Submissions/AVG").once('value').then(function(snapshot) 
-    {
-            avg = snapshot.val();
-    }); 
-    firebase.database().ref("/Foods/" + foods + "/Locations/" + store + "/Submissions/Numppl").once('value').then(function(snapshot) 
-    {
-            numppl = snapshot.val();
-                console.log(avg + " " + numppl)
-        var submit = (avail+(avg*numppl))/(numppl+1);
+
+    firebase.database().ref("/Foods/" + foods + "/Locations/" + store + "/Submissions/AVG").once('value').then(function (snapshot) {
+        avg = snapshot.val();
+    });
+    firebase.database().ref("/Foods/" + foods + "/Locations/" + store + "/Submissions/Numppl").once('value').then(function (snapshot) {
+        numppl = snapshot.val();
+        console.log(avg + " " + numppl)
+        var submit = (avail + (avg * numppl)) / (numppl + 1);
         console.log(submit);
-        numppl = numppl+1;
-        if(avail == 0){
+        numppl = numppl + 1;
+        if (avail == 0) {
             firebase.database().ref("/Foods/" + foods + "/Locations/" + store + "/Submissions").set({
                 AVG: avail,
                 Numppl: 1
-            
+
             });
-        
+
         }
-        
-        else
-        {
+
+        else {
             firebase.database().ref("/Foods/" + foods + "/Locations/" + store + "/Submissions").set({
                 AVG: submit,
                 Numppl: numppl
-            
+
             });
         }
-        });
+    });
 
-    
+
 }
-addFood()
-
+addFood();
